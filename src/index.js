d26eab7cdaa90a926f2a5082235d93698dfb3dea
shamimbhuiyan1 <shamimbhuiyan1508@gmail.com>
2022-05-19 23:40:15 +0600

    page title ,rating component created

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useLayoutEffect } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter } from "react-router-dom";
-
+import { BrowserRouter, useLocation } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+const Wrapper = ({ children }) => {
+  const location = useLocation();
+  useLayoutEffect(() => {
+    document.documentElement.scrollTo(0, 0);
+  }, [location.pathname]);
+  return children;
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <HelmetProvider>
     <BrowserRouter>
-      <App />
+      <Wrapper>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </Wrapper>
     </BrowserRouter>
-  </React.StrictMode>
+  </HelmetProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
